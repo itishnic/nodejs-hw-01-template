060bc3b3fb1fb104c6b98081e09926d7fe060e38
itishnic <122455719+itishnic@users.noreply.github.com>
2024-07-27 10:30:46 +0300

    commit

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,14 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 
 
 export const addOneContact = async () => {
-  const myNewContact = faker.helpers.multiple(createFakeContact, {
-    count: 1,
-  });
+  // const myNewContact = faker.helpers.multiple(createFakeContact, {
+  //   count: 1,
+  // });
+
+  const myNewContact = createFakeContact();
+  // console.log('object :>> ', myNewContact);
   try {
     const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
     const contacts = JSON.parse(data);
